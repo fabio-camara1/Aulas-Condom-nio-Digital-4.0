439328e71a374ea18163ad23a77e045861fd3d37
fabio camara <128440879+fabio-camara1@users.noreply.github.com>
2023-04-24 13:29:53 -0300

    Add files via upload

---
+++ aula_4_CDD.py
@@ -0,0 +1,8 @@
+numero = int(input("digite um numero:"))
+if numero != 0:
+    if numero % 2 == 0:
+        print("o numero é par")
+    else:
+        print("o numero é impar")
+else:
+    print("digite um numero diferente de zero")
