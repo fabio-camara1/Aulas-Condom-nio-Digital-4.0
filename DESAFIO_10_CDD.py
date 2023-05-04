dcddcac5a525184e7cd95fc7434b2c701506de93
fabio camara <128440879+fabio-camara1@users.noreply.github.com>
2023-05-04 13:30:20 -0300

    Add files via upload

---
+++ DESAFIO_10_CDD.py
@@ -0,0 +1,10 @@
+# numero = int(input("digite um numero: "))
+# print("numeros impares de 0 ate", numero)
+# for i in range(numero + 1):
+#    if i % 2 != 0:
+#        print(i)
+
+numero = int(input("digite um numero: "))
+for y in range(1, numero+1):
+    if y % 2 != 0:
+        print(y)
