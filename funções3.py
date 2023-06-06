b19864b09e5dd53d1573c1b6b504767fbde990e4
fabio camara <128440879+fabio-camara1@users.noreply.github.com>
2023-06-06 13:46:38 -0300

    Add files via upload

---
+++ funções3.py
@@ -0,0 +1,9 @@
+def stock():
+    return amount * unitary_value
+
+
+product = input("enter product name:")
+amount = int(input("enter the quantity of products:"))
+unitary_value = float(input("enter the unit value:"))
+
+stock()
