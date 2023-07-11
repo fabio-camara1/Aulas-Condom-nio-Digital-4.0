622d462af8dad5ea53844f2f81620ae9df3af90c
fabio camara <128440879+fabio-camara1@users.noreply.github.com>
2023-07-11 13:09:54 -0300

    Add files via upload

---
+++ Exercicio3.java
@@ -0,0 +1,14 @@
+package Fundamentos;
+
+public class Exercicio3 {
+public static void main(String[] args) {
+	int a = 10;
+	int b = -10;
+	boolean c = true;
+	boolean d = false;
+	System.out.println(~a); // inverte o sinal e subtarai 1
+	System.out.println(~b);  
+	System.out.println(!c); // oposto ao valor booleano
+	System.out.println(!d);
+}
+}
