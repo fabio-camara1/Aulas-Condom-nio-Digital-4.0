d07043b313ad5c003162fdffd3bcc74fc9c8259b
fabio camara <128440879+fabio-camara1@users.noreply.github.com>
2023-07-17 12:49:15 -0300

    Add files via upload

---
+++ Exercicio7.java
@@ -0,0 +1,14 @@
+package Fundamentos;
+
+public class Exercicio7 {
+public static void main(String[] args) {
+	int idade = 18;
+	boolean AmigoDoDono = false;
+	if (idade<18 && AmigoDoDono == false) {
+		System.out.println("não pode entrar");
+	}
+	else {
+		System.out.println("pode entrar");
+	}
+}
+}
